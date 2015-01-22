5a039fac21a1c33401c7961b1b2f1934cdc6fdb1
谢勤 <xieqin@meizu.com>
2015-01-22 17:28:33 +0800

    fix so can not share bug

--- src/sublime_imfix.c
+++ src/sublime_imfix.c
@@ -1,15 +1,15 @@
 /*
 sublime-imfix.c
 Use LD_PRELOAD to interpose some function to fix sublime input method support for linux.
-By Cjacker Huang
- 
-gcc -shared -o libsublime-imfix.so sublime-imfix.c `pkg-config --libs --cflags gtk+-2.0` -fPIC
-LD_PRELOAD=./libsublime-imfix.so subl
+By Cjacker Huang <jianzhong.huang at i-soft.com.cn>
+
+gcc -shared -o libsublime-imfix.so sublime_imfix.c  `pkg-config --libs --cflags gtk+-2.0` -fPIC
+LD_PRELOAD=./libsublime-imfix.so sublime_text
 */
 #include <gtk/gtk.h>
 #include <gdk/gdkx.h>
 typedef GdkSegment GdkRegionBox;
- 
+
 struct _GdkRegion
 {
   long size;
@@ -17,16 +17,16 @@ struct _GdkRegion
   GdkRegionBox *rects;
   GdkRegionBox extents;
 };
- 
+
 GtkIMContext *local_context;
- 
+
 void
 gdk_region_get_clipbox (const GdkRegion *region,
             GdkRectangle    *rectangle)
 {
   g_return_if_fail (region != NULL);
   g_return_if_fail (rectangle != NULL);
- 
+
   rectangle->x = region->extents.x1;
   rectangle->y = region->extents.y1;
   rectangle->width = region->extents.x2 - region->extents.x1;
@@ -35,17 +35,17 @@ gdk_region_get_clipbox (const GdkRegion *region,
   rect.x = rectangle->x;
   rect.y = rectangle->y;
   rect.width = 0;
-  rect.height = rectangle->height;
-  //The caret width is 2;
+  rect.height = rectangle->height; 
+  //The caret width is 2; 
   //Maybe sometimes we will make a mistake, but for most of the time, it should be the caret.
   if(rectangle->width == 2 && GTK_IS_IM_CONTEXT(local_context)) {
         gtk_im_context_set_cursor_location(local_context, rectangle);
   }
 }
- 
+
 //this is needed, for example, if you input something in file dialog and return back the edit area
 //context will lost, so here we set it again.
- 
+
 static GdkFilterReturn event_filter (GdkXEvent *xevent, GdkEvent *event, gpointer im_context)
 {
     XEvent *xev = (XEvent *)xevent;
@@ -56,7 +56,7 @@ static GdkFilterReturn event_filter (GdkXEvent *xevent, GdkEvent *event, gpointe
     }
     return GDK_FILTER_CONTINUE;
 }
- 
+
 void gtk_im_context_set_client_window (GtkIMContext *context,
           GdkWindow    *window)
 {
@@ -65,7 +65,7 @@ void gtk_im_context_set_client_window (GtkIMContext *context,
   klass = GTK_IM_CONTEXT_GET_CLASS (context);
   if (klass->set_client_window)
     klass->set_client_window (context, window);
- 
+
   if(!GDK_IS_WINDOW (window))
     return;
   g_object_set_data(G_OBJECT(context),"window",window);
@@ -75,5 +75,10 @@ void gtk_im_context_set_client_window (GtkIMContext *context,
     gtk_im_context_focus_in(context);
     local_context = context;
   }
-  gdk_window_add_filter (window, event_filter, context);
+  gdk_window_add_filter (window, event_filter, context); 
+}
+void __attribute__ ((constructor)) on_load(void)
+{
+    // Clear `LD_PRELOAD` environment variable
+    putenv("LD_PRELOAD=");
 }
